670d1421f26de9161655a02bbcfd8e3bf3ce55f0
Katorov <katorov@mail.ru>
2021-05-24 21:31:59 +0300

    Create redirect view

--- shortener/views.py
+++ shortener/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, get_object_or_404
 
-# Create your views here.
+from shortener.models import ShortLink
+
+
+def redirect_to_long_url(request, short_link_id):
+    """Перенаправить пользователя по короткой ссылке"""
+    short_link = get_object_or_404(ShortLink, pk=short_link_id)
+    return redirect(short_link.url)
